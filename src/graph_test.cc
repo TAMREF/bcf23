3e1d4688c82e49908d7b8794c5939863a5ee15f1
changki <changki@moloco.com>
2023-06-22 14:27:30 +0900

    test: organize some tests

--- src/graph_test.cc
+++ src/graph_test.cc
@@ -5,18 +5,28 @@
 TEST_CASE("graph_basic_tests", "[graph]") {
     Graph<int> g(size_t(3));
 
-    REQUIRE(g.N() == 3);      
+    SECTION("initialized graph") {
+        REQUIRE(g.N() == 3);
+    }
 
     g.add_vertex();
 
-    // added a vertex
-    REQUIRE(g.N() == 4); 
+    SECTION("add a vertex") {
+        REQUIRE(g.N() == 4); 
+    }
 
-    g.add_edge(Edge<int>({0, 1, 2}));
-    g.add_edge(Edge<int>({1, 2, 1}));
-    g.add_edge(Edge<int>({2, 3, -1}));
+    SECTION("add some edges") {
+        g.add_edge(Edge<int>({0, 1, 2}));
+        g.add_edge(Edge<int>({1, 2, 1}));
+        g.add_edge(Edge<int>({2, 3, -1}));
 
-    REQUIRE(g.M() == 3);
-    REQUIRE(g.get_min_edge_weight() == -1);
+        REQUIRE(g.M() == 3);
+        REQUIRE(g.get_min_edge_weight() == -1);
+        REQUIRE(g.get_weight(Edge<int>({0, 1, 2})) == 2);
+    }
 
+    SECTION("manipulated potential") {
+        g.phi[0] = 1;
+        REQUIRE(g.get_weight(Edge<int>({0, 1, 2})) == 3);
+    }
 }
